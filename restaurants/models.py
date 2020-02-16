05ba591e783cca76eea61219b7a586b1324eb89c
Osama Sider <osamamksider@hotmail.com>
2020-02-16 22:11:22 +0200

    Images for task 8

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+
 class Restaurant(models.Model):
     name = models.CharField(max_length=120)
     description = models.TextField()
     opening_time = models.TimeField()
     closing_time = models.TimeField()
+    logo = models.ImageField(blank=True)
